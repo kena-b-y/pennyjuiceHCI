9ba84165153629b90b1055ad6e631bdae49e3861
Betty Makena <70952884+kena-b-y@users.noreply.github.com>
2022-10-20 12:46:10 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+require('./bootstrap');
+
+import { createApp } from 'vue'
+
+import router from './router/index'
+import App from './App.vue'
+// import './assets/styles/style.scss'
+
+// Import User Class
+import User from './Helpers/User';
+window.User = User
+
+ // Import Notification Class
+ import Notification from './Helpers/Notification';
+ window.Notification = Notification
+
+
+
+ // Sweet Alert start 
+import Swal from 'sweetalert2'
+window.Swal = Swal;
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  onOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+});
+
+window.Toast = Toast;
+ 
+// Sweet Alert End 
+
+ window.Reload = app;
+
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+
